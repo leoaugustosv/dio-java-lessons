a98269197c15665d3f2e943dea72772210a51a22
leoaugustosv <kleeniietv@gmail.com>
2024-04-19 21:30:07 -0300

    feat: create Node class

--- 3-DataStructures/7-CircularLinkedList/src/Node.java
+++ 3-DataStructures/7-CircularLinkedList/src/Node.java
@@ -1,5 +1,33 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Node<T> {
+    private Node<T> next;
+
+    private T data;
+    public Node(T data){
+        this.next = null;
+        this.data = data;
     }
+
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [data=" + data + "]";
+    }
+
+    
+
+    
 }
