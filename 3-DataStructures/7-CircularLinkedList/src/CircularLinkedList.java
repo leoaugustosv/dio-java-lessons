7e4a6c05e4caaabeb096ad42c70e7884a720e688
leoaugustosv <kleeniietv@gmail.com>
2024-04-21 19:52:21 -0300

    feat: create remove() method

--- 3-DataStructures/7-CircularLinkedList/src/CircularLinkedList.java
+++ 3-DataStructures/7-CircularLinkedList/src/CircularLinkedList.java
@@ -3,6 +3,28 @@ public class CircularLinkedList<T> {
     private Node<T> tail;
     private int listSize;
 
+    public void remove(int index){
+        if(index > this.listSize){
+            throw new IndexOutOfBoundsException("Index bigger than the list current size!");
+        }
+
+        Node<T> pointer = this.tail;
+
+        if(index == 0){
+            this.tail = this.tail.getNext();
+            this.head.setNext(this.tail);
+        }
+        else if(index==1){
+            this.tail.setNext(this.tail.getNext().getNext());
+        }
+        else{
+            for (int i = 0; i < index-1; i++) {
+                pointer = pointer.getNext();
+            }
+            pointer.setNext(pointer.getNext().getNext());
+        }
+        this.listSize--;
+    }
 
     public T get(int index){
         return this.getNode(index).getData();
@@ -10,7 +32,7 @@ public class CircularLinkedList<T> {
 
     private Node<T> getNode(int index){
         if(this.isEmpty()){
-            throw new IndexOutOfBoundsException("Empty list!");
+            throw new IndexOutOfBoundsException("List is currently empty!");
         }
 
         if(index==0){
