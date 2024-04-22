f44919509941c9a26cdc4a3172a268a67f41cb33
leoaugustosv <kleeniietv@gmail.com>
2024-04-22 13:28:10 -0300

    feat: create BinaryTree project

---
+++ 3-DataStructures/8-BinaryTree/src/Node.java
@@ -0,0 +1,10 @@
+public class Node<T> {
+    private T data;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node(T data){
+        this.data = data;
+        this.left = this.left = null;
+    }
+}
