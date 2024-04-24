a4e1fb11b5d4fa78a74d52efdb1c237313cb8267
leoaugustosv <kleeniietv@gmail.com>
2024-04-24 16:20:19 -0300

    feat: create complete node Class

--- 3-DataStructures/8-BinaryTree/src/Node.java
+++ 3-DataStructures/8-BinaryTree/src/Node.java
@@ -1,10 +1,40 @@
-public class Node<T> {
+public class Node<T extends Comparable <T>> {
     private T data;
     private Node<T> left;
     private Node<T> right;
 
     public Node(T data){
         this.data = data;
-        this.left = this.left = null;
+        this.left = this.right = null;
     }
+
+    public Node(){
+
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    
 }
