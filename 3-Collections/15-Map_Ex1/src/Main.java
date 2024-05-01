fb5b65d0cb4f4dc2e9d2f388e5579f9cb8bfe9d8
leoaugustosv <kleeniietv@gmail.com>
2024-05-01 19:19:39 -0300

    update main

--- 3-Collections/15-Map_Ex1/src/Main.java
+++ 3-Collections/15-Map_Ex1/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         AgendaContato agenda1 = new AgendaContato();
+        
         agenda1.adicionarContato("Léo", 946990193);
+        agenda1.adicionarContato("Léo", 111444);
         agenda1.adicionarContato("Marcos", 913949548);
         System.out.println(agenda1.pesquisarContato("Marcoss"));
         System.out.println(agenda1.exibirContatos());
