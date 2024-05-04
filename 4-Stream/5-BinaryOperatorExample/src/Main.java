9e07bd3b42de8ae2e63829607fc327cbddfbbcc9
leoaugustosv <kleeniietv@gmail.com>
2024-05-04 14:30:02 -0300

    feat: initial commit

---
+++ 4-Stream/5-BinaryOperatorExample/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<Integer> listaNums = Arrays.asList(3, 6, 10);
+
+        int resultado = listaNums.stream()
+                .reduce(0, (n1, n2) -> n1 + n2);
+
+        System.out.println(resultado);
+    }
+}
