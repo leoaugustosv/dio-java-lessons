89d51ab0857a3eec71a15a48fcfee48852598695
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 19:26:47 -0300

    fix: standardize main class

--- 4-Stream/15-Desafio-AgruparMultiplos3ou5/src/Main.java
+++ 4-Stream/15-Desafio-AgruparMultiplos3ou5/src/Main.java
@@ -1,5 +1,11 @@
-public class App {
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // DESAFIO: Mostrar a lista na ordem númerica
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
     }
 }
