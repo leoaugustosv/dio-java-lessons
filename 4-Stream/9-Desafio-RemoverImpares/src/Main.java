11037a79915f0c2d7c1f000c6b4dadb34d319813
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 20:03:13 -0300

    feat: adicionar solucao

--- 4-Stream/9-Desafio-RemoverImpares/src/Main.java
+++ 4-Stream/9-Desafio-RemoverImpares/src/Main.java
@@ -3,9 +3,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // DESAFIO: Mostrar a lista na ordem númerica
+        // DESAFIO: Remova todos os valores ímpares
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
+        System.out.println(numeros.stream()
+                .filter(num -> num % 2 != 0)
+                .toList());
     }
 }
