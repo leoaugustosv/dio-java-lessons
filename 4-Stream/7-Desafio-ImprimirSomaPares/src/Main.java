b02d02e991a9c5b025841717343b7d4243e2331b
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 19:53:24 -0300

    feat: adicionar solucao

--- 4-Stream/7-Desafio-ImprimirSomaPares/src/Main.java
+++ 4-Stream/7-Desafio-ImprimirSomaPares/src/Main.java
@@ -3,9 +3,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // DESAFIO: Mostrar a lista na ordem númerica
+        // DESAFIO: Imprima a soma dos números pares da lista
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        System.out.println(
+                numeros.stream()
+                        .filter(num -> num % 2 == 0)
+                        .reduce(0, (num1, num2) -> num1 + num2));
 
     }
 }
