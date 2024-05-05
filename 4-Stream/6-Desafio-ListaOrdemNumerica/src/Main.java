c1ac9032d892b0a88a6a9f521d395542daa01928
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 19:18:19 -0300

    feat: solucao do desafio

--- 4-Stream/6-Desafio-ListaOrdemNumerica/src/Main.java
+++ 4-Stream/6-Desafio-ListaOrdemNumerica/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Mostrar a lista na ordem númerica
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numeros.stream().sorted().forEach(System.out::println);
     }
 }
