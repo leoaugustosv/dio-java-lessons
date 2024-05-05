41984b099e9121b7cd90dd409e9e6bf2a4069834
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 19:59:10 -0300

    feat: adicionar solucao

--- 4-Stream/8-Desafio-VerificarPositivos/src/Main.java
+++ 4-Stream/8-Desafio-VerificarPositivos/src/Main.java
@@ -3,9 +3,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // DESAFIO: Mostrar a lista na ordem númerica
+        // DESAFIO: Verifique se todos os números da lista são positivos
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
+        System.out.println(numeros.stream().allMatch(num -> num > 0));
     }
 }
