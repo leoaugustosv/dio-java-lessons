1c99fbd427c629e99b431f6749f59afb0a7b7e04
leoaugustosv <kleeniietv@gmail.com>
2024-05-05 19:19:45 -0300

    update Main.java

--- 4-Stream/6-Desafio-ListaOrdemNumerica/src/Main.java
+++ 4-Stream/6-Desafio-ListaOrdemNumerica/src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Mostrar a lista na ordem númerica
+        // DESAFIO: Mostrar a lista na ordem númerica
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
